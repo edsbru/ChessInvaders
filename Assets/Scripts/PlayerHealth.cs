5dccebf31b17236696299e7d763eabd68bc4d152
PC-Enti-Joan-Martin <invencion@enti.cat>
2023-03-22 11:58:40 +0100

    prueba animacion muerte

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,14 +7,21 @@ public class PlayerHealth : MonoBehaviour
 {
 
     public int health = 3;
+    Animator animator;
 
     public UnityEvent playerDiesEvent = new UnityEvent();
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void RecieveDamage()
     {
         health--;
         if (health <= 0)
         {
+            animator.SetTrigger("Death");
             playerDiesEvent.Invoke();
         }
     }
